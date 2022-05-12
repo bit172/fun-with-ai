8732b2e7e50f1309f1f61a8d8fd81574ebad1d46
Jonathan Hsu <bit172@gmail.com>
2022-05-12 15:51:25 -0400

    feat: add redirect message

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,19 @@ function App() {
       <h1 className="title">Fun with AI</h1>
       <Form setResponses={setResponses} />
       <Responses responses={responses} />
+      <div className="flex flex-col gap-3 fixed justify-center items-center top-0 left-0 w-full h-full bg-black text-white text-6xl">
+        <h1>Moved to:</h1>
+        <a
+          className="block text-4xl hover:underline hover:text-blue-600"
+          target="_self"
+          href="https://627d62302033ef007d0a3fd4--splendid-sopapillas-573766.netlify.app/"
+        >
+          https://627d62302033ef007d0a3fd4--splendid-sopapillas-573766.netlify.app/
+        </a>
+        <h2 className="mt-10">
+          Hosting on gh pages triggers a key rotation on OpenAI 😢
+        </h2>
+      </div>
     </div>
   );
 }
