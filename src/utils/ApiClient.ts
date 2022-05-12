69ca220b002bca58fd0ab4b02d91f26a594e92f2
Jonathan Hsu <bit172@gmail.com>
2022-05-11 23:25:54 -0400

    fix: check for headers presence for subsequent requests

--- src/utils/ApiClient.ts
+++ src/utils/ApiClient.ts
@@ -2,11 +2,13 @@ import { DataBody } from "../interfaces";
 
 const sendPrompt = async (data: DataBody) => {
   const URL = "https://api.openai.com/v1/engines/text-curie-001/completions";
-  ApiClient.setHeader("Content-Type", "application/json");
-  ApiClient.setHeader(
-    "Authorization",
-    `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`
-  );
+  if (!ApiClient.headers.has("Content-Type"))
+    ApiClient.setHeader("Content-Type", "application/json");
+  if (!ApiClient.headers.has("Authorization"))
+    ApiClient.setHeader(
+      "Authorization",
+      `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`
+    );
 
   return ApiClient.sendPrompt(URL, data);
 };
